1a8466ad3d296232a491534c67e3c5ff7e3e8948
eojin-dev <eojin-dev@naver.com>
2021-07-26 16:06:12 +0900

    feat. 타이포그래피, 컬러, 아이콘 추가

---
+++ frontend/public/assets/index.js
@@ -0,0 +1,4 @@
+export { default as calendarIcon } from './calendar.svg';
+export { default as chartIcon } from './chart.svg';
+export { default as checkIcon } from './check.svg';
+export { default as fileTextIcon } from './file-text.svg';
